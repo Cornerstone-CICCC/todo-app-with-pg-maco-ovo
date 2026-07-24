f9834127accc030ec0c7b076d2550df1837cf1ef
makoto2025 <aratamakoto2025@gmail.com>
2026-07-23 20:40:35 -0700

    feat: get todos

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import pool from "./db/pool.ts";
+import { getTodos } from './constrollers/todosControllers.ts'
 
 dotenv.config();
 
@@ -10,14 +11,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/test", async (req, res) => {
-	try {
-		const result = await pool.query("SELECT NOW()");
-		res.json(result.rows[0]);
-	} catch (error) {
-		res.status(500).json({ error: "database connection failed" });
-	}
-});
+// app.get("/test", async (req, res) => {
+// 	try {
+// 		const result = await pool.query("SELECT NOW()");
+// 		res.json(result.rows[0]);
+// 	} catch (error) {
+// 		res.status(500).json({ error: "database connection failed" });
+// 	}
+// });
+
+app.get("/todos", getTodos);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
